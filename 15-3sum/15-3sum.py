5698002b99accf81227bba25dfd7800884cab6fd
ltrain81 <63511851+ltrain81@users.noreply.github.com>
2022-04-19 20:02:09 +0200

    Time: 5736 ms (11.87%), Space: 18.1 MB (43.80%) - LeetHub

---
+++ 15-3sum/15-3sum.py
@@ -0,0 +1,33 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        print(nums)
+        sums = []
+        curSum = 0
+        result = []
+        
+        if len(nums) < 3:
+            return result
+        
+        for i in range(len(nums)-2):
+            number = nums[i]
+            secondIndex = i + 1
+            thirdIndex = len(nums) - 1
+            
+            while secondIndex < thirdIndex:
+                neededSum = 0 - (number + nums[secondIndex])
+                for j in range(secondIndex, thirdIndex):
+                    if neededSum == nums[thirdIndex]:
+                        if [number, nums[secondIndex], nums[thirdIndex]] not in result:
+                            result.append([number, nums[secondIndex], nums[thirdIndex]])
+                    if neededSum > nums[thirdIndex]:
+                        break
+                    thirdIndex -= 1
+                secondIndex += 1
+                
+        return result
+                    
+            
+            
+            
+            
