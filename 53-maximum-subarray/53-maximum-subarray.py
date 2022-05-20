a9ac8c2ba9d50a04525276abbf7d6a75bfec434e
Sukwon Lee <63511851+ltrain81@users.noreply.github.com>
2022-05-20 19:39:14 +0200

    Time: 974 ms (44.28%), Space: 27.9 MB (39.88%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -7,32 +7,3 @@ class Solution:
 
         #print(nums)
         return max(nums)
-        
-        
-        '''
-        n = len(nums)
-        self.maxSum = -math.inf
-        self.sums = [[math.inf for i in range(n)] for j in range(n)]
-        for i in range(n):
-            self.calculateSum(nums, i, n-1)
-        
-        return self.maxSum
-        
-    def calculateSum(self, nums, start, end):
-        #print("Now doing.. " + str(start) + " " + str(end))
-        if end - start == 0:
-            self.sums[start][end] = nums[start]
-            #print(self.sums[start][end])
-            self.maxSum = max(self.maxSum, self.sums[start][end]) 
-            return nums[start]
-        
-        if self.sums[start][end] != math.inf:
-            #print(self.sums[start][end])
-            return self.sums[start][end]
-        
-        self.sums[start][end] = self.calculateSum(nums, start, end-1) + self.calculateSum(nums, end, end)
-
-        self.maxSum = max(self.maxSum, self.sums[start][end])
-        
-        return self.sums[start][end]
-        '''
